a646e783d1472087f2af341e1bccd2d01839fb95
Aleksandrs Bogackins <sashik3580@gmail.com>
2018-06-07 21:49:46 +0300

    about content betta version

--- script/app.js
+++ script/app.js
@@ -1,9 +1,28 @@
 ;
 $(function(){
+	console.clear();
 	console.log('on load.');
 
-	$(window).scroll(function() {
-		var $scrollTop = $(this).scrollTop();
-		
-	})
+	var options = {
+		  useEasing: true, 
+		  useGrouping: true, 
+		  separator: '', 
+		  decimal: '.', 
+	};
+	var stats__coffee = new CountUp('stats__coffee', 0, 4599, 0, 2.5, options),
+	stats__website = new CountUp('stats__website', 0, 21, 0, 2.5, options),
+	stats__clients = new CountUp('stats__clients', 0, 118, 0, 2.5, options),
+	stats__team = new CountUp('stats__team', 0, 8, 0, 3.5, options);
+	if (!stats__coffee.error) {
+		  stats__coffee.start();
+	}
+	if (!stats__website.error) {
+		  stats__website.start();
+	}
+	if (!stats__clients.error) {
+		  stats__clients.start();
+	}
+	if (!stats__team.error) {
+		  stats__team.start();
+	}
 })
